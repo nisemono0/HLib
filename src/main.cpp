38e6ee48e273494b8b2f81e59171c3bb3115e617
nisemono0 <12276968+nisemono0@users.noreply.github.com>
2023-04-20 06:07:50 +0300

    don't load any stylesheet on application startup only use the default style

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,8 @@
 
 int main(int argc, char **argv) {
     QApplication app(argc, argv);
-
-    QFile file(":DarkMaroon.qss");
     
     app.setStyle("Fusion");
-    
-    if (file.exists()) {
-        file.open(QFile::ReadOnly);
-        if (file.isOpen()) {
-            app.setStyleSheet(QString::fromLatin1(file.readAll()));
-            file.close();
-        }
-    } else {
-        app.setStyleSheet("");
-    }
 
     MainWindow mainWin;
     mainWin.show();
