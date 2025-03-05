48c5d16f1515943fb7bc37659c64ebfb50f7b7dc
nisemono0 <12276968+nisemono0@users.noreply.github.com>
2025-03-06 01:59:12 +0200

    Up quazip version

--- qmakeconf.sh
+++ qmakeconf.sh
@@ -9,7 +9,7 @@ DEBUG_DIR="Debug:DESTDIR = debug"
 
 UI_DIR="UI_DIR = src/gui/ui"
 
-INCLUDEPATH="INCLUDEPATH += /usr/include/QuaZip-Qt5-1.4/ src/"
+INCLUDEPATH="INCLUDEPATH += /usr/include/QuaZip-Qt5-1.5/ src/"
 LIBS="LIBS += -lquazip1-qt5"
 
 qmake -project "$MODULES" "$CONFIG" "$RELEASE_DIR" "$DEBUG_DIR" "$UI_DIR" "$INCLUDEPATH" "$LIBS"
